af8992865617f80608fa16be73a3ac788fe1a5de
Toma Tomoe <denysiuk.tina@gmail.com>
2018-09-13 13:58:38 +0300

    setting up semantic-ui, working on the navbar

--- react-client/src/components/headerComponent/navBar.jsx
+++ react-client/src/components/headerComponent/navBar.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
+
 class NavBar extends Component {
   render() {
     return (
       <header>
         <ul id="headerButtons">
-          <li className="navButton"><Link to="">Home</Link></li>
+          <li className="navButton"><Link to="">HOME</Link></li>
+          <li className="navButton"><Link to="">ABOUT</Link></li>
+          <li className="navButton"><Link to="">BLOG</Link></li>
+          <li className="navButton"><Link to="">GALLERY</Link></li>
+          <li className="navButton"><Link to="">CONTACTS</Link></li>
         </ul>
+        <div className="navInfo">
+          <p><Icon name='home' /> Adress: </p>
+          <p><span>@</span> Phone: </p>
+          <p><span>@</span> Working Hours: </p>
+        </div>
       </header>
     )
   }
--- react-client/src/components/homePage.jsx
+++ react-client/src/components/homePage.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 class HomePage extends Component {
   render() {
     return (
-      <div>
+      <div className="homePage">
         Home page
       </div>
     )
